d7db29543f1be7a2dc25c3fbced93ec105bf0276
Vibha Rana <ranavibha02@gmail.com>
2020-04-01 14:01:15 -0700

    Recipe.js(model)

---
+++ backend/models/recipe.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const recipeSchema = new mongoose.Schema({
+    title: {
+       type: String,
+       required: true
+    },
+    userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    servings: {
+        required: true,
+        type: Number
+    },
+    description: {
+        required: true,
+        type: String,
+        minlength: 1,
+        maxlength: 200
+    },
+    directions: {
+        required: true,
+        type: [String]
+    },
+    ingredients: {
+        required: true,
+        type: [String]
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+    
+})
+module.exports = Recipe = mongoose.model('Recipe', UserSchema);
