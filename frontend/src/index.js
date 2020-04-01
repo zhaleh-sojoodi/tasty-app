090225358ed46b086200efa10bb4ac38ff57f5c8
Anthony So <anthony8290@hotmail.com>
2020-04-01 16:59:18 -0700

    Added front end router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Switch>
+        <Route path="/" exact render={props => <Dashboard {...props} />} />
+      </Switch>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
