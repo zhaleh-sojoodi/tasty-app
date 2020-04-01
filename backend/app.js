83a5964e68d255ca3d10834e7923edbe6557cfa9
Vibha Rana <ranavibha02@gmail.com>
2020-04-01 02:44:54 -0700

    user-router code added

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,17 @@ const recipeRoute = require('./routes/recipe-route')
 
 const app = express()
 app.use(bodyParser.json())
+//Init Middleware
+app.use(express.json({ extended : false}))
 
 app.use('/api/user', userRoute)
-app.use('api/recipe', recipeRoute)
+app.use('/api/recipe', recipeRoute)
 
 
 
-mongoose.connect('')
+
+
+mongoose.connect("mongodb+srv://vibharana:7Rs07Kmi3fJK0iRP@cluster0-vkvjv.mongodb.net/test?retryWrites=true&w=majority")
 .then(() => {
     app.listen(5000)
 })
