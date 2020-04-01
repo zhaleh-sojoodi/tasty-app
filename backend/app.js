cbcb2e479de610813398778b9f2a3615320166e0
Vibha Rana <ranavibha02@gmail.com>
2020-04-01 00:32:51 -0700

    routes

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const httpError = require('./models/http-error')
+const userRoute = require('./routes/user-route')
+const recipeRoute = require('./routes/recipe-route')
+const app = express()
+app.use(bodyParser.json())
+// app.use('/api/user', userRoute)
+// app.use('api/recipe', recipeRoute)
+// mongoose.connect('')
+// .then(() => {
+// app.listen(5000)
+// })
+// .catch(err => {
+// console.log(err)
+// })
+app.listen(5000)
