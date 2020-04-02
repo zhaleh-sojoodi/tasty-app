3100ea6fe74558962751b7caf53438fadc9a5e10
Anthony So <anthony8290@hotmail.com>
2020-04-02 00:13:52 -0700

    Updated gitignore Added test data to dashboard to display

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -4,10 +4,72 @@ import NavigationBar from '../components/NavigationBar';
 import Footer from '../components/Footer';
 
 function Dashboard() {
+  var recipes = [
+    {
+      title: "Recipe1",
+      difficulty: "Easy",
+      cookingTime: 20,
+      preparationTime: 20,
+      category: "Breakfast",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId1"
+    },
+    {
+      title: "Recipe2",
+      difficulty: "Medium",
+      cookingTime: 30,
+      preparationTime: 30,
+      category: "Lunch",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" },
+        { ingredient: "Ingredient3" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId2"
+    },
+    {
+      title: "Recipe3",
+      difficulty: "Hard",
+      cookingTime: 40,
+      preparationTime: 40,
+      category: "Breakfast",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" },
+        { ingredient: "Ingredient3" },
+        { ingredient: "Ingredient4" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId3"
+    },
+  ];
+
   return (
     <div>
       <NavigationBar />
       <main className="main">
+        {console.log(recipes)}
+          {recipes.map(function(recipe, index) {
+            return (
+              <ul>
+                <li>{ recipe.title }</li>
+                <li>{ recipe.difficulty }</li>
+                <li>{ recipe.cookingTime } min</li>
+                <li>{ recipe.preparationTime } min</li>
+                <li>{ recipe.category }</li>
+                {recipe.ingredients.map(function(i, index) {
+                  return <li>{ i.ingredient }</li>
+                })}
+                <li>{ recipe.creator }</li>
+
+              </ul>
+            )
+          })}
       </main>
       <Footer />
     </div >
