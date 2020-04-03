d92bdc2b550c135b6ee5ceacc3746dc6fa418285
Anthony So <anthony8290@hotmail.com>
2020-04-03 13:14:26 -0700

    Correct Merge from master

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
 import {
-  Card,
-  CardDeck,
-  CardImg,
-  CardSubtitle,
-  CardTitle,
-  Container,
-  Row
+  Container
 } from 'reactstrap';
 
 import NavigationBar from '../components/NavigationBar';
 import Footer from '../components/Footer';
+import RecipeList from '../components/RecipeList';
 
 function Dashboard() {
-  var recipes = [
+  var mostPopular = [
     {
       title: "Recipe1",
       difficulty: "Easy",
@@ -26,7 +21,9 @@ function Dashboard() {
         { ingredient: "Ingredient2" }
       ],
       addedDate: Date.now,
-      creator: "UserId1"
+      creator: "UserId1",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe2",
@@ -40,7 +37,9 @@ function Dashboard() {
         { ingredient: "Ingredient3" }
       ],
       addedDate: Date.now,
-      creator: "UserId2"
+      creator: "UserId2",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe3",
@@ -55,7 +54,9 @@ function Dashboard() {
         { ingredient: "Ingredient4" }
       ],
       addedDate: Date.now,
-      creator: "UserId3"
+      creator: "UserId3",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe1",
@@ -68,7 +69,9 @@ function Dashboard() {
         { ingredient: "Ingredient2" }
       ],
       addedDate: Date.now,
-      creator: "UserId1"
+      creator: "UserId1",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe2",
@@ -82,7 +85,9 @@ function Dashboard() {
         { ingredient: "Ingredient3" }
       ],
       addedDate: Date.now,
-      creator: "UserId2"
+      creator: "UserId2",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe3",
@@ -97,7 +102,9 @@ function Dashboard() {
         { ingredient: "Ingredient4" }
       ],
       addedDate: Date.now,
-      creator: "UserId3"
+      creator: "UserId3",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe1",
@@ -110,7 +117,9 @@ function Dashboard() {
         { ingredient: "Ingredient2" }
       ],
       addedDate: Date.now,
-      creator: "UserId1"
+      creator: "UserId1",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe2",
@@ -124,7 +133,43 @@ function Dashboard() {
         { ingredient: "Ingredient3" }
       ],
       addedDate: Date.now,
-      creator: "UserId2"
+      creator: "UserId2",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    }
+  ];
+
+  var highestRated = [
+    {
+      title: "Recipe1",
+      difficulty: "Easy",
+      cookingTime: 20,
+      preparationTime: 20,
+      category: "Breakfast",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId1",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      title: "Recipe2",
+      difficulty: "Medium",
+      cookingTime: 30,
+      preparationTime: 30,
+      category: "Lunch",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" },
+        { ingredient: "Ingredient3" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId2",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe3",
@@ -139,7 +184,9 @@ function Dashboard() {
         { ingredient: "Ingredient4" }
       ],
       addedDate: Date.now,
-      creator: "UserId3"
+      creator: "UserId3",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
     {
       title: "Recipe1",
@@ -152,43 +199,89 @@ function Dashboard() {
         { ingredient: "Ingredient2" }
       ],
       addedDate: Date.now,
-      creator: "UserId1"
+      creator: "UserId1",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      title: "Recipe2",
+      difficulty: "Medium",
+      cookingTime: 30,
+      preparationTime: 30,
+      category: "Lunch",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" },
+        { ingredient: "Ingredient3" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId2",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      title: "Recipe3",
+      difficulty: "Hard",
+      cookingTime: 40,
+      preparationTime: 40,
+      category: "Breakfast",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" },
+        { ingredient: "Ingredient3" },
+        { ingredient: "Ingredient4" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId3",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      title: "Recipe1",
+      difficulty: "Easy",
+      cookingTime: 20,
+      preparationTime: 20,
+      category: "Breakfast",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId1",
+      image:
+      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      title: "Recipe2",
+      difficulty: "Medium",
+      cookingTime: 30,
+      preparationTime: 30,
+      category: "Lunch",
+      ingredients: [
+        { ingredient: "Ingredient1" },
+        { ingredient: "Ingredient2" },
+        { ingredient: "Ingredient3" }
+      ],
+      addedDate: Date.now,
+      creator: "UserId2",
+      image:
+        "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     }
   ];
 
   return (
     <div>
       <NavigationBar />
-      <Container>
-        <main className="main">
-          {console.log(recipes)}
-          <Row>
-            {recipes.map(function (recipe, index) {
-              return (
-                <CardDeck className="p-1 m-auto">
-                  <Card>
-                    <CardImg top width="100%" alt="Card image cap" />
-                    <CardTitle>{recipe.title}</CardTitle>
-                    <CardSubtitle>Difficulty: {recipe.difficulty}</CardSubtitle>
-                    <CardSubtitle>Cooking Time: {recipe.cookingTime} min</CardSubtitle>
-                    <CardSubtitle>Prep Time: {recipe.preparationTime} min</CardSubtitle>
-                    <CardSubtitle>Category: {recipe.category}</CardSubtitle>
-                    {/* <CardBody>
-                      Ingredients:
-                      {recipe.ingredients.map(function (i, index) {
-                        return <p>{index + 1}. {i.ingredient}</p>
-                      })}
-                    </CardBody> */}
-
-                    <p>Created By: {recipe.creator}</p>
-                  </Card>
-                </CardDeck>
-              )
-            })}
-          </Row>
-        </main>
-      </Container>
-
+      <main className="main">
+        <Container className="mt-4 mb-4">
+          <h4 className="display-4 mb-0">Most Popular</h4>
+          <RecipeList props={mostPopular} />
+        </Container>
+        <Container className="mt-4 mb-4">
+          <h4 className="display-4 mb-0">Highest Rated</h4>
+          <RecipeList props={highestRated} />
+        </Container>
+      </main>
       <Footer />
     </div >
   );
