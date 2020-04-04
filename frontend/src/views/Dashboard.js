5714bb5537d63713b4a5904f5bea691e37d7a5e5
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-03 22:43:45 -0700

    Import user dummy data and add Create Recipe button
    Also added 'Button' component from reactstrap, and a welcome message for the dashboard.

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import {
-  Container
-} from 'reactstrap';
+import { Container, Button } from "reactstrap";
 
-import NavigationBar from 'components/NavigationBar';
-import Footer from 'components/Footer';
-import RecipeDisplay from 'components/RecipeDisplay';
+import NavigationBar from "components/NavigationBar";
+import Footer from "components/Footer";
+import RecipeDisplay from "components/RecipeDisplay";
 
-import { mostPopular, highestRated } from '../dummydata';
+import { mostPopular, highestRated, user } from "../dummydata";
 
 function Dashboard() {
   return (
     <div>
       <NavigationBar />
       <main className="main">
+        <Container className="mt-4 mb-4 d-flex justify-content-between">
+        <h1 className="display-3">Welcome back, {user.name}!</h1>
+          <Button
+            className="mb-4"
+            size="lg"
+            color="default"
+            type="button"
+          >
+            Create Recipe
+          </Button>
+        </Container>
+
         <Container className="mt-4 mb-4">
           <h4 className="display-4 mb-0">Most Popular</h4>
           <RecipeDisplay props={mostPopular} />
           <hr />
         </Container>
+
         <Container className="mt-4 mb-4">
           <h4 className="display-4 mb-0">Highest Rated</h4>
           <RecipeDisplay props={highestRated} />
