c5fd72ea44101895e09af8cf99824131feb9baf3
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-03 22:18:01 -0700

    Import recipe dummy data

--- frontend/src/views/Recipe.js
+++ frontend/src/views/Recipe.js
@@ -1,62 +1,19 @@
 import React, { useState, useEffect } from "react";
-import Ratings from "react-ratings-declarative";
 
+import Ratings from "react-ratings-declarative";
 import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 
 import { Button, Container, Row, Col } from "reactstrap";
 
+import { mostPopular } from "../dummydata";
+
 function Recipe(props) {
-  let recipe = {
-    title: "Healthy Pesto Pasta",
-    description:
-      "Rich and creamy pesto pasta with tomatoes, whole wheat noodles, bright pesto, and greek yogurt. Super creamy and rich pesto sauce made with protein-packed greek yogurt! So amazing you'll ditch store-bought versions for good!",
-    difficulty: "Easy",
-    cookingTime: 10,
-    preparationTime: 20,
-    servings: 2,
-    category: "Dinner",
-    ingredients: [
-      "1 bag whole wheat pasta",
-      "1 cup sliced spinach",
-      "3 tomatoes",
-      "3/4 cup plain greek yogurt",
-      "1 jar (7 oz) genovese pesto",
-      "Extra virgin olive oil, to taste",
-    ],
-    steps: [
-      "Melt butter in a large skillet over medium high heat. Add garlic and cook, stirring frequently, until fragrant, about 1-2 minutes.",
-      "Stir in chicken broth, milk and fettuccine; season with salt and pepper, to taste.",
-      "Bring to a boil; reduce heat and simmer, stirring occasionally, until pasta is cooked through, about 18-20 minutes. Stir in Parmesan. If the mixture is too thick, add more milk as needed until desired consistency is reached.",
-      "Serve immediately, garnished with parsley, if desired.",
-    ],
-    ratings: {
-      averageRating: 4.5,
-      ratings: [
-        {
-          user: "UserId1",
-          rating: 4,
-        },
-        {
-          user: "UserId2",
-          rating: 5,
-        },
-      ],
-    },
-    likes: [
-      {
-        user: "UserId1",
-      },
-      {
-        user: "UserId2",
-      },
-    ],
-    addedDate: Date.now,
-    creator: "UserId1",
-    image:
-      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-  };
+
   console.log(props.location.state);
+
+  let recipe = mostPopular[0];
+
   let creator = {
     name: "Jane Doe",
     image:
@@ -117,7 +74,7 @@ function Recipe(props) {
                 {/* Preparation Time */}
                 <Col sm="6">
                   <Row>
-                    <i className="col-1 ni ni-scissors m-1" />
+                    <i className="col-1 ni ni-scissors m-1 recipe-details-icon" />
                     <Col>
                       <strong>Prep Time</strong>
                       <p>{recipe.preparationTime} minutes</p>
@@ -128,7 +85,7 @@ function Recipe(props) {
                 {/* Difficulty */}
                 <Col sm="6">
                   <Row>
-                    <i className="col-1 ni ni-settings-gear-65 m-1" />
+                    <i className="col-1 ni ni-settings-gear-65 m-1 recipe-details-icon" />
                     <Col>
                       <strong>Difficulty</strong>
                       <p>{recipe.difficulty}</p>
@@ -139,7 +96,7 @@ function Recipe(props) {
                 {/* Cooking Time */}
                 <Col sm="6">
                   <Row>
-                    <i className="col-1 ni ni-time-alarm m-1" />
+                    <i className="col-1 ni ni-time-alarm m-1 recipe-details-icon" />
                     <Col>
                       <strong>Cooking Time</strong>
                       <p>{recipe.cookingTime} minutes</p>
@@ -150,7 +107,7 @@ function Recipe(props) {
                 {/* Servings */}
                 <Col sm="6">
                   <Row>
-                    <i className="col-1 ni ni-single-02 m-1" />
+                    <i className="col-1 ni ni-single-02 m-1 recipe-details-icon" />
                     <Col>
                       <strong>Servings</strong>
                       <p>{recipe.servings} people</p>
@@ -161,7 +118,7 @@ function Recipe(props) {
                 {/* Total Time */}
                 <Col sm="6">
                   <Row>
-                    <i className="col-1 ni ni-single-02 m-1" />
+                    <i className="col-1 ni ni-single-02 m-1 recipe-details-icon" />
                     <Col>
                       <strong>Total Time</strong>
                       <p>
@@ -174,7 +131,7 @@ function Recipe(props) {
                 {/* Category */}
                 <Col sm="6">
                   <Row>
-                    <i className="col-1 ni ni-folder-17 m-1" />
+                    <i className="col-1 ni ni-folder-17 m-1 recipe-details-icon" />
                     <Col>
                       <strong>Category</strong>
                       <p>{recipe.category}</p>
