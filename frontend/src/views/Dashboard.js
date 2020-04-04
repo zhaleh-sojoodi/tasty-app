04ccedbc96e51a2dc06106981ba09578220ba9cd
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-04 03:49:56 -0700

    Make Container tag parent for all children

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -7,35 +7,31 @@ import NavigationBar from "components/NavigationBar";
 import Footer from "components/Footer";
 import RecipeDisplay from "components/RecipeDisplay";
 
-import { mostPopular, highestRated, user } from "../dummydata";
+import { mostPopular, highestRated } from "../dummydata";
 
 function Dashboard() {
   return (
     <div>
       <NavigationBar />
       <main className="main">
-        <Container className="mt-4 mb-4 d-flex justify-content-between">
-        <h1 className="display-3">Welcome back, {user.name}!</h1>
-          <Button
-            className="mb-4"
-            size="lg"
-            color="default"
-            type="button"
-            tag={Link}
-            to="/create-recipe"
-          >
-            Create Recipe
-          </Button>
-        </Container>
+        <Container className="mt-4 mb-5">
+          <section className="d-flex justify-content-end">
+            <Button
+              className="mb-4"
+              color="default"
+              type="button"
+              tag={Link}
+              to="/create-recipe"
+            >
+              Create New
+            </Button>
+          </section>
 
-        <Container className="mt-4 mb-4">
-          <h4 className="display-4 mb-0">Most Popular</h4>
+          <h3 className="display-4 mt-3 mb-4"><i className="ni ni-favourite-28 text-danger mr-2" />Most Popular</h3>
           <RecipeDisplay props={mostPopular} />
           <hr />
-        </Container>
 
-        <Container className="mt-4 mb-4">
-          <h4 className="display-4 mb-0">Highest Rated</h4>
+          <h3 className="display-4 mt-3 mb-4"><i className="ni ni-satisfied text-danger mr-2" />Highest Rated</h3>
           <RecipeDisplay props={highestRated} />
         </Container>
       </main>
