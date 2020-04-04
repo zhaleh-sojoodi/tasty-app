526ab1389880baca3f380970285f4d2fcaf2a918
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-03 21:19:52 -0700

    Remove unneeded component in layout

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -135,51 +135,6 @@ function Profile() {
                       </a>
                     </div>
                   </Col>
-
-                  {/* Connect and Message Buttons */}
-                  {/* <Col
-                    className="order-lg-3 text-lg-right align-self-lg-center"
-                    lg="4"
-                  >
-                    <div className="card-profile-actions py-4 mt-lg-0">
-                      <Button
-                        className="mr-4"
-                        color="info"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                        size="sm"
-                      >
-                        Connect
-                      </Button>
-                      <Button
-                        className="float-right"
-                        color="default"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                        size="sm"
-                      >
-                        Message
-                      </Button>
-                    </div>
-                  </Col> */}
-
-                  {/* Friends, Photos, Comments Counters */}
-                  {/* <Col className="order-lg-1" lg="4">
-                    <div className="card-profile-stats d-flex justify-content-center">
-                      <div>
-                        <span className="heading">22</span>
-                        <span className="description">Friends</span>
-                      </div>
-                      <div>
-                        <span className="heading">10</span>
-                        <span className="description">Photos</span>
-                      </div>
-                      <div>
-                        <span className="heading">89</span>
-                        <span className="description">Comments</span>
-                      </div>
-                    </div>
-                  </Col> */}
                 </Row>
 
                 {/* User Details */}
