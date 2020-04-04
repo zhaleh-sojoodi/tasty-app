22a07f59f0eeba98f0735871c5abb27a30b56164
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-04 03:51:43 -0700

    Link creator name/image to profile, add Edit/Delete recipe options

--- frontend/src/views/Recipe.js
+++ frontend/src/views/Recipe.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import Ratings from "react-ratings-declarative";
 import NavigationBar from "../components/NavigationBar";
@@ -10,7 +10,6 @@ import { Button, Container, Row, Col } from "reactstrap";
 import { mostPopular } from "../dummydata";
 
 function Recipe(props) {
-
   console.log(props.location.state);
 
   let recipe = mostPopular[0];
@@ -52,7 +51,7 @@ function Recipe(props) {
               {/* Title & Like Recipe*/}
               <div className="d-flex justify-content-between">
                 {/* Title */}
-                <h2 className="mt-3">{recipe.title}</h2>
+                <h3 className="mt-3 display-4">{recipe.title}</h3>
 
                 {/* Like Button */}
                 <Button
@@ -63,9 +62,7 @@ function Recipe(props) {
                   <span className="btn-inner--icon">
                     <i className="ni ni-favourite-28" />
                   </span>
-                  <span className="btn-inner--text">
-                    {recipe.likes.length}
-                  </span>
+                  <span className="btn-inner--text">{recipe.likes.length}</span>
                 </Button>
               </div>
               <hr className="mt-2 mb-3" />
@@ -169,43 +166,33 @@ function Recipe(props) {
               {/* Author Details */}
               <div className="p-3 mb-4 shadow">
                 {/* Creator Name */}
-                <h2 className="text-center">{creator.name}</h2>
+                <Link to="/profile">
+                  <h3 className="mt-3 display-4 text-center">{creator.name}</h3>
+                </Link>
 
                 {/* Creator Profile Picture */}
-                <img
-                  style={{
-                    borderRadius: "50%",
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                    objectPosition: "35% 100%",
-                  }}
-                  className="img-fluid mx-auto d-block mb-3"
-                  src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                  alt={creator.name}
-                />
+                <Link to="/profile">
+                  <img
+                    style={{
+                      borderRadius: "50%",
+                      width: "150px",
+                      height: "150px",
+                      objectFit: "cover",
+                      objectPosition: "35% 100%",
+                    }}
+                    className="img-fluid mx-auto d-block mb-3"
+                    src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    alt={creator.name}
+                  />
+                </Link>
 
                 {/* Creator Bio */}
                 <p className="text-center">{creator.bio}</p>
-
-                {/* View Creator Profile */}
-                <Button
-                  className="btn-icon btn-3 mx-auto d-block"
-                  color="danger"
-                  type="button"
-                  tag={Link}
-                  to="/profile"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="ni ni-single-02" />
-                  </span>
-                  <span className="btn-inner--text">View Profile</span>
-                </Button>
               </div>
 
               {/* Ratings */}
               <div className="p-3 shadow d-flex flex-column justify-content-center align-items-center">
-                <h2>Rate this recipe</h2>
+                <h3 className="mt-3 display-4 text-center">Rate this recipe</h3>
                 <Ratings
                   rating={averageRating}
                   widgetRatedColors="#f5365c"
@@ -218,8 +205,29 @@ function Recipe(props) {
                   <Ratings.Widget widgetDimension="25px" />
                 </Ratings>
                 <p className="mt-3">
-                  {recipe.ratings.averageRating} average from {recipe.ratings.ratings.length} votes
+                  {recipe.ratings.averageRating} average from{" "}
+                  {recipe.ratings.ratings.length} votes
                 </p>
+              </div>
+
+              {/* Edit Recipe (Only when the recipe belongs to the current user) */}
+              <div className="mt-5 mb-3 d-flex flex-column justify-content-center align-items-center">
+                <Link to="/edit-recipe" className="text-muted">
+                  Edit recipe
+                </Link>
+              </div>
+
+              {/* Delete Recipe (Only when the recipe belongs to the current user) */}
+              <div className="d-flex flex-column justify-content-center align-items-center">
+                <Link
+                  to="/"
+                  className="text-danger"
+                  onClick={() =>
+                    alert(`Are you sure you want to delete ${recipe.title}?`)
+                  }
+                >
+                  Delete recipe
+                </Link>
               </div>
             </Col>
           </Row>
