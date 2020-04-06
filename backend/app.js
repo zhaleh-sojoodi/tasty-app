8d2bf1385a94b5b2e81a3a6d095cef3e5f91048f
anthonyyso <55161590+anthonyyso@users.noreply.github.com>
2020-04-05 21:51:20 -0700

    Revert "Vibharana"

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,52 @@
+const fs = require('fs')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 const httpError = require('./models/http-error')
 const userRoute = require('./routes/user-route')
 const recipeRoute = require('./routes/recipe-route')
+
+dotenv.config()
 const app = express()
 app.use(bodyParser.json())
-// app.use('/api/user', userRoute)
-// app.use('api/recipe', recipeRoute)
-// mongoose.connect('')
-// .then(() => {
-// app.listen(5000)
-// })
-// .catch(err => {
-// console.log(err)
-// })
-app.listen(5000)
+
+//enable Cors
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH')
+    next();
+});
+
+app.use('/api/user', userRoute)
+app.use('/api/recipe', recipeRoute)
+
+//handle 404 error
+app.use((req,res,next) => {
+    const error = new httpError('Could not find the route' , 404);
+    return next(error);
+})
+
+app.use((error,req,res,next) => {
+    if (req.file) {
+        fs.unlink (req.file.path, err => {
+            console.log(err)
+        })
+    }
+    if(res.headerSent) {
+        return next(error)
+    }
+    res.status(error.code||500) 
+    res.json({message:error.message || 'An unknown message occured'})
+});
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(process.env.DB_CONN)
+.then(() => {
+    app.listen(5000)
+})
+.catch(err => {
+    console.log(err)
+})
+
