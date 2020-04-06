29b00a7df7a17231640213d91df06b9f4de063ae
anthonyyso <55161590+anthonyyso@users.noreply.github.com>
2020-04-05 21:49:14 -0700

    Revert "Vibharana"

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,9 +3,20 @@ const uniqueValidator = require('mongoose-unique-validator')
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+<<<<<<< HEAD
+    
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique : true,
+       
+       
+=======
     name: {
         type: String,
         required: true
+>>>>>>> 43e44e39a7963bb3c6cd1ed8bcf574b2ab7f837e
     },
     email: {
         type: String,
@@ -17,9 +28,20 @@ const userSchema = new Schema({
         required: true, 
         minlength : 6
     },
+<<<<<<< HEAD
+   /* profile: {
+        type: String,
+        required: true
+    },*
+    resetPasswordLink: {
+      data: String,
+      default: ''  
+    },*/
+=======
     biography: {
         type: String
     },
+>>>>>>> 43e44e39a7963bb3c6cd1ed8bcf574b2ab7f837e
     recipes : [{
         type : mongoose.Types.ObjectId, 
         require : true, ref : 'Recipe'
@@ -27,7 +49,13 @@ const userSchema = new Schema({
     likes : [{
         type: mongoose.Types.ObjectId,
         ref : 'Recipe'
+<<<<<<< HEAD
+    }],
+    resetPasswordToken: String,// used for after password reset is submitted
+	  resetPasswordExpires: Date
+=======
     }]
+>>>>>>> 43e44e39a7963bb3c6cd1ed8bcf574b2ab7f837e
     
 });
 
