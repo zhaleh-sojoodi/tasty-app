bfc2cb5552391c6fa047daced59c1ad97217f452
Anthony So <anthony8290@hotmail.com>
2020-04-07 00:39:12 -0700

    Added connection for myRecipes

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -11,30 +11,43 @@ import {
   Row
 } from 'reactstrap';
 
+const BASE_URL = "http://localhost:5000/api/user";
+const USER_ID = 'user_id';
+
 function RecipeList(props) {
+  // console.log('--from recipeList');
+  // console.log(props.props);
+  // console.log('--from recipeList');
+
+  useEffect(() => {
+    const getUser = async() => {
+      const settings = {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      }
+      let userId = sessionStorage.getItem(USER_ID);
+
+      const uri = BASE_URL + "/" + userId;
+  
+      try {
+        const response = await fetch(uri, settings);
+        let data = await response.json();
+       console.log(data);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+    props.props.forEach((recipe) => {
+      getUser();
+    })
+  }, []);
+
   return (
     <ListGroup>
-      {props.props.map(function (recipe, index) {
-        return (
-          // {/* State to be changed to ObjectID */ }
-          <Link to={{ pathname: "/recipe", state: recipe.creator }} key={index}>
-            <ListGroupItem className="mb-3" tag="button" action >
-              <Row>
-                <Col sm="12" md="4" lg="3">
-                  <img width="200" src={recipe.image} alt="..." className="img-thumbnail float-left"></img>
-                </Col>
-                <Col sm="12" md="8" lg="9">
-                  <CardTitle>{recipe.title}</CardTitle>
-                  <CardSubtitle>{recipe.description}</CardSubtitle>
-                  <CardText>
-                    <small className="text-muted">Created By: {recipe.creator}</small>
-                  </CardText>
-                </Col>
-              </Row>
-            </ListGroupItem>
-          </Link>
-        )
-      })}
+
     </ListGroup >
   );
 }
--- frontend/src/views/MyRecipes.js
+++ frontend/src/views/MyRecipes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Container, Button } from 'reactstrap';
@@ -9,9 +9,36 @@ import RecipeList from 'components/RecipeList';
 
 import { mostPopular } from "../dummydata";
 
-function MyRecipes() {
+const BASE_URL = "http://localhost:5000/api/recipe";
+const USER_ID = 'user_id';
 
-  let myRecipes = mostPopular;
+function MyRecipes() {
+  const [recipes, setRecipes] = useState();
+  
+  useEffect(() => {
+    const getMyRecipes = async() => {
+      const settings = {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      }
+      let userId = sessionStorage.getItem(USER_ID);
+
+      const uri = BASE_URL + "/user/" + userId;
+  
+      try {
+        const response = await fetch(uri, settings);
+        let data = await response.json();
+        setRecipes(data.recipes);
+        console.log(data.recipes);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+    getMyRecipes();
+  }, []);
 
   return (
     <div>
@@ -29,9 +56,8 @@ function MyRecipes() {
             Create New
           </Button>
         </Container>
-
         <Container>
-          <RecipeList props = {myRecipes} />
+          {recipes && <RecipeList props = {recipes} /> }
         </Container>
         </main>
       <Footer />
