ff1d723b17b76fcc9d2f4ffa548d81284099b33d
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-08 05:19:57 -0700

    Change user_email to user_name

--- frontend/src/utils/forceLogout.js
+++ frontend/src/utils/forceLogout.js
@@ -1,6 +1,6 @@
 const forceLogout = _ => {
     sessionStorage.removeItem('auth_token');
-    sessionStorage.removeItem('user_email');
+    sessionStorage.removeItem('user_name');
     sessionStorage.removeItem('user_id');
     window.location.reload(true);
     alert("Something went wrong. Please sign in again.");
