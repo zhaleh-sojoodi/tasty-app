6eea042e6a603f1fafdeee97cdc361ae2f72eabb
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-08 08:38:06 -0700

    Update Liked.js with get user's liked recipes endpoint
    Closes #63

--- frontend/src/views/Liked.js
+++ frontend/src/views/Liked.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import forceLogout from "../utils/forceLogout";
 
 import { Container } from 'reactstrap';
 
 import NavigationBar from 'components/NavigationBar';
 import Footer from 'components/Footer';
 import RecipeList from 'components/RecipeList';
+import RecipeGrid from 'components/RecipeGrid';
 
-import { mostPopular } from "../dummydata";
-
-// const BASE_URL = "http://localhost:5000/api/";
-// const USER_ID = 'user_id';
+const BASE_URL = "http://localhost:5000/api";
+const USER_ID = 'user_id';
 
 function Liked(props) {
-  let likedRecipes = mostPopular;
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = async(id) => {
+    const settings = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    // Get user's liked recipes
+    try {
+      const uri = BASE_URL + "/recipe/user/all/list/like/recipes/" + id;
+
+      const response = await fetch(uri, settings);
+
+      // No recipes found
+      if(response.status == 500) {
+        console.log("Unable to get user's liked recipes.");
+        return;
+      }
+
+      // Successful fetch, get user's liked recipes
+      let data = await response.json();
+      setRecipes(data.likedRecipes);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    if(sessionStorage.getItem(USER_ID)) {
+      fetchRecipes(sessionStorage.getItem(USER_ID));
+    } else {
+      forceLogout();
+    }
+  }, [])
 
   // const [recipes, setRecipes] = useState([]);
   
@@ -59,8 +96,11 @@ function Liked(props) {
     <main className="main">
     <Container className="mt-4 mb-4">
       <h2 className="display-3 mb-3">Liked Recipes</h2>
-      <RecipeList props = {likedRecipes} />
-      <hr />
+      {
+      recipes.length ? <RecipeGrid props={recipes && recipes} />
+      : <p className="lead">You haven't liked any recipes yet <span role="img" aria-label="Sad Emoji">😔</span></p>
+      }
+      
     </Container>
     </main>
     <Footer />
