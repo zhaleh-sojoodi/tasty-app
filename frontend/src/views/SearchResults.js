876d21a84582b2509b239813a00c9f8c14e7f8ff
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-08 05:22:42 -0700

    Create SearchResults.js

---
+++ frontend/src/views/SearchResults.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+
+import { Container } from 'reactstrap';
+
+import NavigationBar from 'components/NavigationBar';
+import Footer from 'components/Footer';
+import RecipeList from 'components/RecipeList';
+import { mostPopular } from '../dummydata';
+
+const BASE_URL = "http://localhost:5000/api/";
+
+function SearchResults(props) {
+
+  const [query, setQuery] = useState();
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = async(query) => {
+    const settings = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    // Get user data
+    try {
+      const uri = BASE_URL + "recipe/all/recipes/search/title/" + query;
+      const response = await fetch(uri, settings);
+
+      // Unable to fetch data, profile does not exist
+      if(!response.ok) {
+        console.error("Unable to get recipes.");
+        return;
+      }
+
+      // Successful fetch, get user data
+      let data = await response.json();
+      console.log(`${data.recipes.length} recipes found for ${query}`);
+      console.log(data.recipes);
+      setRecipes(data.recipes);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    console.log(`Received: "${props.location.state.query}"`)
+    setQuery(props.location.state.query);
+    fetchRecipes(encodeURIComponent(props.location.state.query));
+  }, [])
+
+  return (
+    <>
+    <NavigationBar {...props} />
+    <main className="main">
+    <Container className="mt-4 mb-4">
+    <h2 className="display-3 mb-3">Search Results for '{query && query}'</h2>
+      <RecipeList props={ recipes && recipes } />
+    </Container>
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default SearchResults;
