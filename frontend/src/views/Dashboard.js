8c3a4db08ffca7d9a0b2e3b83ac65f6a62e85d75
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-08 05:20:22 -0700

    Give props to NavigationBar, rename RecipeDisplay to RecipeGrid

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -5,11 +5,11 @@ import { Container, Button } from "reactstrap";
 
 import NavigationBar from "components/NavigationBar";
 import Footer from "components/Footer";
-import RecipeDisplay from "components/RecipeDisplay";
+import RecipeGrid from "components/RecipeGrid";
 
 const BASE_URL = "http://localhost:5000/api/recipe/all";
 
-function Dashboard() {
+function Dashboard(props) {
   const [mostPopular, setMostPopular] = useState();
   const [highestRated, setHighestRated] = useState();
 
@@ -47,8 +47,8 @@ function Dashboard() {
   }, []);
 
   return (
-    <div>
-      <NavigationBar />
+    <>
+      <NavigationBar {...props} />
       <main className="main">
         <Container className="mt-4 mb-5">
           <section className="d-flex justify-content-end">
@@ -64,15 +64,15 @@ function Dashboard() {
           </section>
 
           <h3 className="display-4 mt-3 mb-4"><i className="ni ni-favourite-28 text-danger mr-2" />Most Popular</h3>
-            {mostPopular && <RecipeDisplay props={mostPopular} /> }
+            {mostPopular && <RecipeGrid props={mostPopular} /> }
           <hr />
 
           <h3 className="display-4 mt-3 mb-4"><i className="ni ni-satisfied text-danger mr-2" />Highest Rated</h3>
-            {highestRated && <RecipeDisplay props={highestRated} /> }
+            {highestRated && <RecipeGrid props={highestRated} /> }
         </Container>
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
